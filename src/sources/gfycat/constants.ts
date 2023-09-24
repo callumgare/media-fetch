6957cb516c8edd40134a2ab8214ffebcab94f761
Callum Gare <callum@gare.au>
2023-09-24 19:39:42 +1000

    Remove gfycat

--- src/sources/gfycat/constants.ts
+++
@@ -1,3 +0,0 @@
-export const rootUrlApi = "https://api.gfycat.com/v1";
-export const rootUrlSite = "https://gfycat.com";
-export const sourceName = "Gfycat";
--- src/sources/index.ts
+++ src/sources/index.ts
@@ -1,2 +1,1 @@
-export { default as gfycat } from "./gfycat/index.js";
 export { default as giphy } from "./giphy/index.js";
