6498d8a52da4039eb460dfe83d1764dd7242bfe1
Callum Gare <callum@gare.au>
2024-07-14 00:38:49 +1000

    feat: export MediaFinderQuery

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import MediaFinderQuery, { MediaFinderQueryProps } from "./MediaFinderQuery.js";
 
 export {default as MediaFinder} from "./MediaFinder.js";
+export {default as MediaFinderQuery} from "./MediaFinderQuery.js";
 
 export function createMediaFinderQuery(props: MediaFinderQueryProps): MediaFinderQuery {
   return new MediaFinderQuery(props);
